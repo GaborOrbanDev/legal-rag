fef3e3f51e4114a947be22fed9dbbdf370864def
Orbán Gábor <118307686+GaborOrbanDev@users.noreply.github.com>
2025-01-21 16:05:58 +0100

    Refactor Self-RAG workflow to include output state and enhance document retrieval and answer generation logic

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 # %%
 import yaml
+from typing import Literal
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
-from langchain_core.messages import SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import ChatOllama, OllamaEmbeddings
@@ -34,15 +34,18 @@ vectorstore = FAISS.load_local(
 class InputState(BaseModel):
     question: str
 
-
-class SelfRAGState(InputState):
-    question: str
+class OutputState(BaseModel):
     documents: list[Document] = []
     generation: str = ""
-    try_count: int = 0
+
+class SelfRAGState(InputState, OutputState):
+    retrieve_count: int = 0
+    generation_count: int = 0
 
 # %%
 class SelfRAG:
+    """Self-RAG workflow for generating answers based on retrieved and self-checked documents."""
+
     def __init__(self, vectorstore: FAISS, max_try_count: int = 3):
         self.vectorstore = vectorstore
         self.max_try_count = max_try_count
@@ -53,37 +56,56 @@ class SelfRAG:
             self.prompts = yaml.safe_load(file)
 
     def create_rag(self) -> CompiledStateGraph:
-        workflow = StateGraph(SelfRAGState, input=InputState)
+        """Creates Self-RAG workflow"""
+
+        # Create the workflow
+        workflow = StateGraph(SelfRAGState, input=InputState, output=OutputState)
+
+        # Adding nodes
         workflow.add_node("retrieve", self.retrieve)
         workflow.add_node("grade_and_filter", self.grade_and_filter)
-        workflow.add_node("generate", self.generate)
         workflow.add_node("rewrite_question", self.rewrite_question)
+        workflow.add_node("generate", self.generate)
+        workflow.add_node("no_answer", self.no_answer)
+
+        # Adding edges
         workflow.add_edge(START, "retrieve")
         workflow.add_edge("retrieve", "grade_and_filter")
         workflow.add_conditional_edges(
             "grade_and_filter", 
             self.did_find_relevant_documents, 
             {
-                True: "generate",
-                False: "rewrite_question"
+                "generate": "generate",
+                "rewrite": "rewrite_question",
+                "no_answer": "no_answer"
             }
         )
         workflow.add_conditional_edges(
             "generate",
             self.did_find_answer,
             {
-                True: END,
-                False: "rewrite_question"
+                "end": END,
+                "generate": "generate",
+                "no_answer": "no_answer"
             }
         )
         workflow.add_edge("rewrite_question", "retrieve")
+        workflow.add_edge("no_answer", END)
+
         return workflow.compile()
+    
+    # --------------------------------------------------------------------------------------------------------
+    # region: Node functions
 
     def retrieve(self, state: SelfRAGState) -> SelfRAGState:
+        """Retrieves semantically similar documents from the database."""
+
         documents = self.vectorstore.similarity_search(query=state.question, k=10)
-        return {"documents": documents, "try_count": state.try_count + 1}
+        return {"documents": documents, "retrieve_count": state.retrieve_count + 1}
     
     def grade_and_filter(self, state: SelfRAGState) -> SelfRAGState:
+        """Grades the retrieved documents and filters out the irrelevant ones."""
+
         class DocumentGrade(BaseModel):
             """Grade the document based on its relevance to the question."""
             grade: int = Field(..., description="Give a score between 0 and 10 to the document based on its relevance to the question.")
@@ -111,14 +133,9 @@ class SelfRAG:
 
         return {"documents": filtered_documents}
     
-    def generate(self, state: SelfRAGState) -> SelfRAGState:
-        template = HumanMessagePromptTemplate.from_template(self.prompts["generation_prompt"])
-        doc_text = [doc.page_content for doc in state.documents]
-        message = template.format(**{"question": state.question, "context": doc_text})
-        generation = self.llm.invoke([message]).content
-        return {"generation": generation}
-    
     def rewrite_question(self, state: SelfRAGState) -> SelfRAGState:
+        """Rewrites original question to get a better answer."""
+
         regeneation_chain = (
             ChatPromptTemplate.from_messages([
                 ("system", self.prompts["question_rewrite_system_prompt"]),
@@ -129,39 +146,58 @@ class SelfRAG:
         )
         return {"question": regeneation_chain.invoke({"question": state.question})}
     
-    def did_find_relevant_documents(self, state: SelfRAGState) -> bool:
-        return bool(state.documents)
+    def generate(self, state: SelfRAGState) -> SelfRAGState:
+        """Generates an answer based on the retrieved documents."""
+
+        template = HumanMessagePromptTemplate.from_template(self.prompts["generation_prompt"])
+        doc_text = [doc.page_content for doc in state.documents]
+        message = template.format(**{"question": state.question, "context": doc_text})
+        generation = self.llm.invoke([message]).content
+        return {"generation": generation, "generation_count": state.generation_count + 1}
     
-    def did_find_answer(self, state: SelfRAGState) -> bool:
+    def no_answer(self, state: SelfRAGState) -> SelfRAGState:
+        """Returns no answer if no relevant documents were found or the generation limit was reached."""
+
+        return {"generation": "No relevant or factual answer was found.", "documents": []}
+    
+    # endregion
+    # --------------------------------------------------------------------------------------------------------
+    # region: Conditional edge functions
+     
+    def did_find_relevant_documents(self, state: SelfRAGState) -> Literal["generate", "rewrite", "no_answer"]:
+        """Routes the workflow to generate an answer if found at least one relevant documents, else rewrite the question or return no answer."""
+
+        if state.retrieve_count >= self.max_try_count and len(state.documents) == 0:
+            return "no_answer"
+        elif len(state.documents) > 0:
+            return "generate"
+        else:
+            return "rewrite"
+    
+    def did_find_answer(self, state: SelfRAGState) -> Literal["end", "generate", "no_answer"]:
+        """Checks if the generated answer is factual or not and routes the workflow accordingly."""
+
         class DidNotHalucinate(BaseModel):
             """Grade whether the answer is based on the retrieved documents or not."""
             grade: bool = Field(..., description="Say True if the answer is factual and based on the retrieved documents or False if the answer is not factual and not based on the retrieved documents.")
-
-        class DidResolveQuestion(BaseModel):
-            """Grade the answer whether it is an answer to the question or not."""
-            grade: bool = Field(..., description="Say True if the answer is an answer to the question or False if the answer is not an answer to the question.")
 
         factuality_checker_chain = (
             ChatPromptTemplate.from_messages([("user", self.prompts["halucination_detection_prompt"])])
             | self.grader_llm.with_structured_output(DidNotHalucinate, method="json_schema")
         )
 
-        answer_grader_chain = (
-            ChatPromptTemplate.from_messages([("user", self.prompts["answer_grader_prompt"])])
-            | self.grader_llm.with_structured_output(DidResolveQuestion, method="json_schema")
-        )
-
         doc_text = [doc.page_content for doc in state.documents]
         did_pass_halucination_test: DidNotHalucinate = factuality_checker_chain.invoke({"answer": state.generation, "context": doc_text})
         if did_pass_halucination_test.grade:
-            answer_grade: DidResolveQuestion = answer_grader_chain.invoke({"question": state.question, "answer": state.generation})
-            if answer_grade.grade:
-                return True
-            elif state.try_count < self.max_try_count:
-                return False
-            else:
-                raise Exception("Did not find an answer.")
-        return False
+            return "end"
+        elif state.generation_count >= self.max_try_count:
+            return "no_answer"
+        else:
+            return "generate"
+        
+    # endregion
+    # --------------------------------------------------------------------------------------------------------
+        
 
 # %%
 agent = SelfRAG(vectorstore).create_rag()
