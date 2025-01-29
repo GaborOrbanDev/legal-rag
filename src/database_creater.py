abb5fc6fae7519e2d2b0774c61eee23e86726574
Orbán Gábor <118307686+GaborOrbanDev@users.noreply.github.com>
2025-01-29 15:41:48 +0100

    create new databases

--- src/database_creater.py
+++ src/database_creater.py
@@ -7,13 +7,13 @@ from document_processor import pdf_loader, split_documents
 
 # %% Loading Document(s)
 documents = pdf_loader("./files/us_constitution.pdf")
-documents = split_documents(documents, 1000, 100)
+documents = split_documents(documents, 500, 0)
 
 
 # %% Creating Vector Store
 vector_store = FAISS.from_documents(
     documents=documents,
-    embedding=OllamaEmbeddings(model="llama3.1:latest")
+    embedding=OllamaEmbeddings(model="nomic-embed-text:latest")
 )
 
-vector_store.save_local("database", index_name="us_constitution_1000_100")
+vector_store.save_local("database", index_name="us_constitution_500_0_net_spec_sep")
